f62211bebe19a68b814bd70cd3de1df2cd16cabd
Somya Subham Dash <workforssd@gmail.com>
2023-05-17 22:10:05 +0530

    changed sidebar

--- web/src/components/layout/layoutAlt.js
+++ web/src/components/layout/layoutAlt.js
@@ -1,11 +1,11 @@
 import { Outlet } from "react-router-dom";
-import { Sidebar } from "./sidebar";
+
 import { SidebarAlt } from "./sidebarAlt";
 import { ToastContainer } from "react-toastify";
-export default function Layout() {
+export default function LayoutAlt() {
   return (
     <div className="flex flex-row h-screen w-full">
-      <Sidebar />
+      <SidebarAlt />
       <main className="w-5/6 max-h-screen overflow-y-auto">
         <Outlet></Outlet>
       </main>
