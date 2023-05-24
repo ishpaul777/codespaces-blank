9650cb17adbab35c52ca768bf8f13ce054f963ce
surajmn1 <suraj.natraj@factly.in>
2023-05-24 18:53:02 +0530

    add uppy

---
+++ web/src/components/Modal/index.js
@@ -0,0 +1,37 @@
+import React, { useEffect } from 'react';
+import { AiOutlineClose } from 'react-icons/ai';
+
+function Modal({ open, onClose, children , closeButton=true}) {
+  useEffect(() => {
+    const handleEscapeKey = (event) => {
+      if (event.keyCode === 27 && open) {
+        onClose();
+      }
+    };
+
+    document.addEventListener('keydown', handleEscapeKey);
+
+    return () => {
+      document.removeEventListener('keydown', handleEscapeKey);
+    };
+  }, [open, onClose]);
+
+  return (
+    <>
+      {open && (
+       <div
+       className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 "
+     >
+        <div className="bg-white rounded-lg shadow-lg p-4"> 
+        {closeButton&&<div className="pb-5" onClick={onClose}>
+            <AiOutlineClose className="ml-auto"size={20} />
+            </div>}
+        <div className="p-4 box-border"> {children}</div>
+        </div>
+     </div>
+      )}
+    </>
+  );
+}
+
+export default Modal;
--- web/src/pages/personas/create.js
+++ web/src/pages/personas/create.js
@@ -1,11 +1,49 @@
 import { RiShareBoxLine } from "react-icons/ri";
+import UppyUploader from "../../components/Uppy";
+import { useState } from "react";
+import Modal from "../../components/Modal";
+import { AiOutlineDelete } from 'react-icons/ai';
+
+function Link({ to, children, className , onDelete }) {
+  return (
+    <div className="flex items-center">
+      <a href={to} target="_blank" rel="noopener noreferrer" className={className}>
+        {children}
+      </a>
+      <AiOutlineDelete onClick={onDelete} className="ml-2 text-red-500" />
+    </div>
+  );
+}
+
 
 export default function CreatePersona() {
+  const [avatar, setAvatar] = useState("");
+  const [open, setOpen] = useState(false);
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [prompt, setPrompt] = useState('');
+  const [visibility, setVisibility] = useState("private");
+  const onDelete = () => {
+    setAvatar("");
+  };
+  const handleUpload = (url) => {
+    setAvatar(url);
+    setOpen(false);
+  };
+  const handleModalClose = () => {
+    setOpen(false);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  
+    console.log('Form submitted:', { name, description, prompt, visibility , avatar});
+  };
   return (
+    <form onSubmit={handleSubmit}>
     <div className="my-5 mx-10">
       <div className="flex flex-row justify-between my-3">
         <h1 className="text-[24px] font-semibold">Create Persona</h1>
-        <button className="text-[14px] font-semibold bg-black-50 py-[9px] px-[20px] text-white rounded-lg">
+        <button type="submit" className="text-[14px] font-semibold bg-black-50 py-[9px] px-[20px] text-white rounded-lg">
           Create and chat
         </button>
       </div>
@@ -20,6 +58,8 @@ export default function CreatePersona() {
           <input
             className="w-[40%] px-[14px] py-[10px] border border-[#D0D5DD] rounded-lg"
             placeholder="Enter name here"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
         </div>
         <div className="flex flex-row flex-wrap justify-between py-10 border-b border-[#EAECF0]">
@@ -27,6 +67,8 @@ export default function CreatePersona() {
           <textarea
             className="w-[40%] px-[14px] py-[10px] h-[84px] border border-[#D0D5DD] rounded-lg resize-none"
             placeholder="Enter description here"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
           />
         </div>
         <div className="flex flex-row flex-wrap justify-between items-center py-10 border-b border-[#EAECF0]">
@@ -48,6 +90,8 @@ export default function CreatePersona() {
           <textarea
             className="w-[40%] px-[14px] py-[10px] h-[84px] border border-[#D0D5DD] rounded-lg resize-none"
             placeholder="Enter prompt here"
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
           />
         </div>
         <div className="flex flex-row flex-wrap justify-between items-center py-10 border-b border-[#EAECF0]">
@@ -57,12 +101,17 @@ export default function CreatePersona() {
               Who is allowed to talk to them?
             </p>
           </div>
-          <select className="w-[40%] px-[14px] py-[10px] border border-[#D0D5DD] rounded-lg text-[#667085]">
-            <option disabled selected value="">
+          <select 
+           value={visibility}
+           onChange={(e) => setVisibility(e.target.value)}
+           className="w-[40%] px-[14px] py-[10px] border border-[#D0D5DD] rounded-lg text-[#667085]">
+            {/* <option disabled
+            //  selected 
+             value="">
               Public: Anyone can chat
-            </option>
-            <option value="option1">Option 1</option>
-            <option value="option2">Option 2</option>
+            </option> */}
+            <option value="private">Private: Only you can chat </option>
+            <option value="public"> Public: Anyone can chat</option>
           </select>
         </div>
         <div className="flex flex-row flex-wrap justify-between items-center py-10">
@@ -72,18 +121,21 @@ export default function CreatePersona() {
               You can either create an image from text or upload an image.
             </p>
           </div>
-          <div className="flex flex-row gap-5 items-center">
-            <button className="text-[14px] font-semibold bg-black-50 py-[9px] px-[20px] text-white rounded-lg">
+          {!avatar?<div className="flex flex-row gap-5 items-center">
+            <button onClick={()=>setOpen(true)} className="text-[14px] font-semibold bg-black-50 py-[9px] px-[20px] text-white rounded-lg">
               Create Image
             </button>
             <h1 className="text-[16px] font-normal text-[#667085]">or</h1>
             <input
+              onClick={(e) => {e.preventDefault();setOpen(true)}}
               type="file"
               className="text-sm text-[#667085] border border-[#D0D5DD] rounded-lg file:border-0 file:p-[10px] file:font-normal file:text-[#667085] file:bg-[#F4F4F4]"
             />
-          </div>
+          </div>:<Link to={avatar} onDelete={onDelete} className="text-blue-500">View Avatar</Link>}
         </div>
       </div>
+      <Modal open={open} onClose={handleModalClose}><UppyUploader onUpload={handleUpload}></UppyUploader></Modal> 
     </div>
+    </form>
   );
 }
