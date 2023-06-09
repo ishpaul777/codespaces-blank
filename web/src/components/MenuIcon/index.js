dd81aeeed5f33d5db532d27bb9f8ed0c1e2af0c7
ishpaul207 <ishpaul777@gmail.com>
2023-06-09 12:39:53 +0000

    fix responsiveness for sidbar

---
+++ web/src/components/MenuIcon/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const MenuIcon = () => {
+  return (
+    <svg width="45" height="45" viewBox="0 0 45 45" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g clipPath="url(#clip0_6015_267)">
+        <path d="M11.4199 31.749H36.9199V28.5824H11.4199V31.749ZM19.4199 23.8324H36.9199V20.6657H19.4199V23.8324ZM11.4199 12.749V15.9157H36.9199V12.749H11.4199Z" fill="#1E1E1E"/>
+      </g>
+      <defs>
+        <clipPath id="clip0_6015_267">
+          <rect width="44" height="44" fill="white" transform="translate(0.669922 0.249023)"/>
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+
+export default MenuIcon;
--- web/src/components/layout/layout.js
+++ web/src/components/layout/layout.js
@@ -1,15 +1,42 @@
 import { Outlet } from "react-router-dom";
 import { Sidebar } from "./sidebar";
 import { SidebarAlt } from "./sidebarAlt";
+import { useState } from "react";
 import { ToastContainer } from "react-toastify";
+import useWindowSize from "../../hooks/useWindowSize";
+import MenuIcon from "../MenuIcon";
+
 export default function Layout() {
+  const { isMobileScreen } = useWindowSize()
+  const [sideBarOpen, setSidebarOpen] = useState(!isMobileScreen)
+
   return (
-    <div className="flex flex-row h-screen w-full">
-      <Sidebar />
-      <main className="w-5/6 max-h-screen overflow-y-auto">
+    <div className={`${isMobileScreen ? "w-full flex-wrap" : ""} flex flex-row h-screen max-h-screen overflow-y-auto`}>
+      <div className="w-1/6">
+        {
+          isMobileScreen &&
+          <nav className="w-screen flex justify-between items-end fixed top-0 bg-[#DCE4E7] px-8 pb-4 pt-10">
+            <img
+              src="https://images.factly.in/login/applications/logos/factly.png?rs:fill/h:60"
+              className="cursor-pointer w-[123px] h-[32px]"
+              alt="logo"
+            />
+            <div
+              onClick={() => setSidebarOpen(true)}
+            >
+              <MenuIcon className="w-8 h-8" />
+            </div>
+          </nav>
+        }
+        <Sidebar
+          sideBarOpen={sideBarOpen}
+          setSidebarOpen={setSidebarOpen}
+        />
+      </div>
+      <main className={`${isMobileScreen ? "mt-10 w-full" : "w-5/6 max-h-screen overflow-y-auto"}`}>
         <Outlet></Outlet>
       </main>
       <ToastContainer />
-    </div>
+    </div >
   );
 }
