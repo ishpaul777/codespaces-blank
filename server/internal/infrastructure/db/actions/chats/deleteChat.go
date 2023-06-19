35415110f8ff6e7ddc9d641fd55d170cfaa6a5c8
elliot14A <akshithkatkuri14@gmail.com>
2023-06-19 17:13:28 +0530

    feat: added error handling to chat and chat_collection

--- server/internal/infrastructure/db/actions/chats/deleteChat.go
+++ server/internal/infrastructure/db/actions/chats/deleteChat.go
@@ -1,7 +1,17 @@
 package chats
 
-import "github.com/factly/tagore/server/internal/domain/models"
+import (
+	"github.com/factly/tagore/server/internal/domain/constants/custom_errors"
+	"github.com/factly/tagore/server/internal/domain/models"
+	"gorm.io/gorm"
+)
 
 func (p *PGChatsRepository) DeleteChat(userID, chatID uint) error {
-	return p.client.Where("created_by_id = ? AND id = ?", userID, chatID).Delete(&models.Chat{}).Error
+	err := p.client.Where("created_by_id = ? AND id = ?", userID, chatID).Delete(&models.Chat{}).Error
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return custom_errors.ErrNotFound
+		}
+	}
+	return nil
 }
--- server/internal/infrastructure/db/actions/chats/updateChatCol.go
+++ server/internal/infrastructure/db/actions/chats/updateChatCol.go
@@ -11,14 +11,14 @@ func (p *PGChatsRepository) UpdateChatColByID(userID, colID uint, name string) e
 	err := p.client.Model(&models.ChatCollection{}).Where("created_by_id = ? AND id = ?", userID, colID).Find(&models.ChatCollection{}).Error
 	if err != nil {
 		if err == gorm.ErrRecordNotFound {
-			return custom_errors.ChatCollectionNotFound
+			return custom_errors.ErrNotFound
 		}
 		return err
 	}
 
-	exists := p.ChatCollectionNameExists(name)
+	exists := p.ChatCollectionNameExists(name, &colID)
 	if exists {
-		return custom_errors.ChatCollectionNameExists
+		return custom_errors.ErrNameExists
 	}
 
 	err = p.client.Model(&models.ChatCollection{}).Where("created_by_id = ? AND id = ?", userID, colID).Updates(map[string]interface{}{"name": name}).Error
