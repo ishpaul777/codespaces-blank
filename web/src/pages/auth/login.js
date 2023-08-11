505460d1b6a7a2d1c41b47db4f9dd44f2ebd9f8f
vsumit89 <sumit.vishwakarma@factly.in>
2023-08-11 18:06:42 +0530

    added 2fa login

--- web/src/pages/auth/login.js
+++ web/src/pages/auth/login.js
@@ -17,8 +17,11 @@ function Login({ title }) {
     },
   });
 
+  const [totp, setTOTP] = useState('');
+
   const [aal2, setaal2] = useState(false);
   const [ui, setUI] = useState({});
+
 
   const handleFieldChange = (e) => {
     let { name, value } = e.target;
@@ -128,9 +131,7 @@ function Login({ title }) {
     }
   }, []);
 
-  const handleLogin = (e) => {
-    e.preventDefault();
-
+  const handleLogin = () => {
     var authForm = document.createElement("form");
     authForm.action = ui.action;
     authForm.method = ui.method;
@@ -199,79 +200,151 @@ function Login({ title }) {
     oidcForm.submit();
   };
 
+
+  const handleTOTPSubmit = () => {
+    var totpForm = document.createElement('form');
+    totpForm.action = ui.action;
+    totpForm.method = ui.method;
+    totpForm.style.display = 'none';
+
+    var csrfInput = document.createElement('input');
+    csrfInput.name = 'csrf_token';
+    csrfInput.type = 'hidden';
+    csrfInput.value = ui.nodes.find(
+      (value) => value.attributes.name === 'csrf_token',
+    ).attributes.value;
+
+    var totpMethod = document.createElement('input');
+    totpMethod.name = 'method';
+    totpMethod.value = 'totp';
+
+    var totpInput = document.createElement('input');
+    totpInput.name = 'totp_code';
+    totpInput.value = totp;
+
+    totpForm.appendChild(csrfInput);
+    totpForm.appendChild(totpMethod);
+    totpForm.appendChild(totpInput);
+
+    document.body.appendChild(totpForm);
+
+    totpForm.submit();
+  };
+
   return (
     <>
       <h1 className="text-center text-3xl font-semibold">{title}</h1>
       <div className="bg-[#F3F5F8] w-full flex flex-col gap-4">
-        {ui?.messages?.[0]?.text && (
-          <span
-            className={`
+        {aal2 ? (
+          <>
+            {ui?.messages?.[0]?.text && (
+              <span
+                className={`
             border p-2 text-sm rounded-md
           ${
             ui?.messages?.[0]?.type === "error"
               ? " bg-red-100 text-red-500 border-red-500"
               : "bg-green-100 text-green-500 border-green-500"
           }`}
-          >
-            {getErrorMsgByCode(ui?.messages?.[0]?.id)}
-          </span>
+              >
+                {getErrorMsgByCode(ui?.messages?.[0]?.id)}
+              </span>
+            )}
+
+            <Input
+              label='Enter TOTP'
+              type={"input" }
+              name={"totp"}
+              backgroundColor="white"
+              placeholder={"Enter your TOTP"}
+              onChange={(e) => {
+                const { value } = e.target;
+                setTOTP(value);
+              }}
+              padding="p-3"
+              initialValue={totp.value}
+              error={totp.error}
+            />
+            <button
+              onClick={() => handleTOTPSubmit()}
+              className="block w-full bg-[#1E1E1E] hover:bg-slate-800 py-4 rounded-lg text-white font-semibold mb-2 shadow-md"
+            >
+              Submit
+            </button>
+          </>
+        ) : (
+          <>
+            {ui?.messages?.[0]?.text && (
+              <span
+                className={`
+            border p-2 text-sm rounded-md
+          ${
+            ui?.messages?.[0]?.type === "error"
+              ? " bg-red-100 text-red-500 border-red-500"
+              : "bg-green-100 text-green-500 border-green-500"
+          }`}
+              >
+                {getErrorMsgByCode(ui?.messages?.[0]?.id)}
+              </span>
+            )}
+            <Input
+              label={"Email"}
+              name={"email"}
+              type={"input"}
+              initialValue={fields.email.value}
+              error={fields.email.error}
+              onChange={handleFieldChange}
+              backgroundColor="white"
+              placeholder={"Enter your email"}
+              padding="p-3"
+            ></Input>
+            <Input
+              label={"Password"}
+              name={"password"}
+              type={"password"}
+              initialValue={fields.password.value}
+              error={fields.password.error}
+              onChange={handleFieldChange}
+              placeholder={"Enter your password"}
+              padding="p-3"
+            />
+            <div className="flex flex-row-reverse text-blue-600">
+              <Link to={"/auth/recovery"}>Forgot Password?</Link>
+            </div>
+            <button
+              onClick={handleSubmit}
+              className="block w-full bg-[#1E1E1E] hover:bg-slate-800 py-4 rounded-lg text-white font-semibold mb-2 shadow-md"
+            >
+              Sign in
+            </button>
+            <div className="flex items-center justify-between gap-2 w-full">
+              <hr className="h-px w-2/5 mx-auto bg-gray-300  border-0" />
+              <p className="text-gray-500 text-center font-semibold">or</p>
+              <hr className="h-px w-2/5 mx-auto bg-gray-300  border-0" />
+            </div>
+            <button
+              className="flex gap-3 justify-center items-center w-full bg-white hover:bg-gray-100 py-4 rounded-lg text-[#1E1E1E] font-semibold mb-2 shadow-md"
+              onClick={handleSignInWithGoogle}
+            >
+              <img src={GoogleIcon} alt="google icon" />
+              Sign in with Google
+            </button>
+            <span className="text-gray-500 text-sm text-center w-full font-semibold">
+              Don't have an account?
+              <Link
+                to="/auth/registration"
+                className="text-[#1E1E1E] font-semibold ml-2"
+              >
+                Sign up
+              </Link>
+            </span>
+            <span className="text-gray-500 text-sm text-center w-full font-semibold">
+              {ui?.messages?.[0]?.id === 4000010 && (
+                <Link to={"/auth/verification"}>Verify your account</Link>
+              )}
+            </span>
+          </>
         )}
-        <Input
-          label={"Email"}
-          name={"email"}
-          type={"input"}
-          initialValue={fields.email.value}
-          error={fields.email.error}
-          onChange={handleFieldChange}
-          backgroundColor="white"
-          placeholder={"Enter your email"}
-          padding="p-3"
-        ></Input>
-        <Input
-          label={"Password"}
-          name={"password"}
-          type={"password"}
-          initialValue={fields.password.value}
-          error={fields.password.error}
-          onChange={handleFieldChange}
-          placeholder={"Enter your password"}
-          padding="p-3"
-        />
-        <div className="flex flex-row-reverse text-blue-600">
-          <Link to={"/auth/recovery"}>Forgot Password?</Link>
-        </div>
-        <button
-          onClick={handleLogin}
-          className="block w-full bg-[#1E1E1E] hover:bg-slate-800 py-4 rounded-lg text-white font-semibold mb-2 shadow-md"
-        >
-          Sign in
-        </button>
-        <div className="flex items-center justify-between gap-2 w-full">
-          <hr className="h-px w-2/5 mx-auto bg-gray-300  border-0" />
-          <p className="text-gray-500 text-center font-semibold">or</p>
-          <hr className="h-px w-2/5 mx-auto bg-gray-300  border-0" />
-        </div>
-        <button
-          className="flex gap-3 justify-center items-center w-full bg-white hover:bg-gray-100 py-4 rounded-lg text-[#1E1E1E] font-semibold mb-2 shadow-md"
-          onClick={handleSignInWithGoogle}
-        >
-          <img src={GoogleIcon} alt="google icon" />
-          Sign in with Google
-        </button>
-        <span className="text-gray-500 text-sm text-center w-full font-semibold">
-          Don't have an account?
-          <Link
-            to="/auth/registration"
-            className="text-[#1E1E1E] font-semibold ml-2"
-          >
-            Sign up
-          </Link>
-        </span>
-        <span className="text-gray-500 text-sm text-center w-full font-semibold">
-          {ui?.messages?.[0]?.id === 4000010 && (
-            <Link to={"/auth/verification"}>Verify your account</Link>
-          )}
-        </span>
       </div>
     </>
   );
